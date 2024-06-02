c34e850a4e2b21beb0543c6e7ebab835a4929185
PraneetBandi123 <praneetbandi2002@gmail.com>
2024-06-01 22:34:54 -0400

    Create Ball constructor with initial properties

--- cc_6.js
+++ cc_6.js
@@ -1,34 +1,27 @@
 //JavaScript file (script. js)
- const canvas =ducment.getElementById(`canvas`);
- const ctx=canvas.getcontext(`2d`);
+const canvas = ducment.getElementById(`canvas`);
+const ctx = canvas.getcontext(`2d`);
 
 
 
 class Ball {
-    constructor (x,y,radius,dx,dy,color) {
-        this.x=x;
-        this.y=y;
-        this.radius=radius;
-        this.dx=dx;
-        this.dy=dy;
-        this.color=color;
+    constructor(x, y, radius, dx, dy, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.dx = dx;
+        this.dy = dy;
+        this.color = color;
 
     }
 }
 
+draw(); {
+        ctx.beginPath();
+    ctx.arc(this.x,this.y,this.radius,0.2 * Math.PI);
+    ctx.fillStyle=this.color;
+    ctx.fill();
+
+}
 
 
-    
-
-
-
-
-    
-
-
-
-
-
-    
-
-    
